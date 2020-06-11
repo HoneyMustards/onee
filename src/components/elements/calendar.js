57b2a8f4dd0097bf21191d46d060e6d8cdcf3d98
aydncnar <cinar.aydinn@gmail.com>
2020-06-12 00:58:16 +0300

    Calendar üzerinde çalışıldı

--- src/components/elements/calendar.js
+++ src/components/elements/calendar.js
@@ -6,10 +6,20 @@ const localizer = momentLocalizer(moment)
 const eventItem = (props) => {
     return (
         <div>
-            <img className="calender-event-image" src={props.event.imageUrl} /> {props.event.title}
+            {typeof props.event.price == 'undefined' &&
+             <div className="reserved ">
+                <img className="calender-event-image" src={props.event.imageUrl} /> {props.event.title}
+             </div>
+            }
+            {typeof props.event.price != 'undefined' &&
+                <div className="price">
+                    <p className="calender-event-price">{props.event.price}</p>
+                </div>
+            }
         </div>
     )
 }
+
 
 const CalenderOnee = (props) => {
     console.log(props.events);
@@ -20,6 +30,7 @@ const CalenderOnee = (props) => {
             endAccessor="end"
             events={props.events}
             style={props.style}
+            selectable={true}
             components={{
                 event: eventItem,
             }}
